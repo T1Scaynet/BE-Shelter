d1cb35dbc76dbff1bd1add5add35cc33801a67cf
joacorojasmoschini <joaco.vla@gmail.com>
2023-03-13 13:24:57 -0300

    db conection

---
+++ src/db.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+// String para la conexion con MongoDB Atlas
+const connection = `mongodb+srv://admin:${process.env.DB_PASSWORD}@db-refugio.lmdbpnj.mongodb.net/test`
+
+const mongoDB = () => {
+  mongoose.connect(connection, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+  })
+    .then(() => {
+      console.log('db connected')
+    })
+    .catch(error => {
+      console.error(error)
+    })
+}
+
+mongoose.set('strictQuery', false) // Este set es para evitar errores de version de mongoose
+mongoDB()
