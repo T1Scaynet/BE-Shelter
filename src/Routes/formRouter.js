8c2ff687fc49384414e753c7c5106a2944befff6
Gustavo Chura <gustavochura94@gmail.com>
2023-03-16 17:43:19 -0300

    fix: the endpoint form was removed

--- src/Routes/formRouter.js
+++ src/Routes/formRouter.js
@@ -3,10 +3,10 @@ const { postForm, deleteForm, getAllForms, getAForm } = require('../Controllers/
 
 const router = Router();
 
-router.post('/form', postForm); // mandar el form
-router.delete('/form/:id', deleteForm); // borrar un form
-router.get('/form', getAllForms); // todos los form
-router.get('/form/:id', getAForm); // un form
+router.post('/', postForm); // mandar el form
+router.delete('/:id', deleteForm); // borrar un form
+router.get('/', getAllForms); // todos los form
+router.get('/:id', getAForm); // un form
 
 // router.get('/', pruebaGet)
 
