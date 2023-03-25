9358525721e643e27be3a4b97ba6509ca5304d12
MadridSelene <seleneailenmadrid@gmail.com>
2023-03-25 11:25:49 -0300

    formContact-routes-casi crud completo

--- src/Routes/formRouter.js
+++ src/Routes/formRouter.js
@@ -1,13 +1,19 @@
 const { Router } = require('express');
 const { postForm, deleteForm, getAllForms, getAForm } = require('../Controllers/formController');
+const { postContactForm, getAllContactForms, getAContactForm, deleteContactForm } = require('../Controllers/contactForm');
 const { authToken, isAdmin } = require('../Middlewares/authToken');
 
 const router = Router();
 
 // CLIENT --
+router.post('/contact', postContactForm); // form para contactarse
 router.post('/', postForm);
 
 // ADMIN --
+router.get('/contact/:id', getAContactForm); // forms para contactarse
+router.get('/contact', getAllContactForms); // forms para contactarse
+router.delete('/contact/:id', deleteContactForm); // forms para contactarse
+
 router.delete('/:id', authToken, isAdmin, deleteForm);
 router.get('/', authToken, isAdmin, getAllForms);
 router.get('/:id', authToken, isAdmin, getAForm);
