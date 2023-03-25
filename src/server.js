b2ef6039d27605c86c65567b8062309c8047d938
Ramiro Aduviri <Kevin_95_ah@hotmail.com.ar>
2023-03-25 00:13:45 -0300

    chore: eslint fixes

--- src/server.js
+++ src/server.js
@@ -2,16 +2,15 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
-const mercadopago = require('mercadopago')
+const mercadopago = require('mercadopago');
 
-
-mercadopago.configure({access_token:process.env.MERCADOPAGO_KEY})
+mercadopago.configure({ access_token: process.env.MERCADOPAGO_KEY });
 
 const petRoute = require('./Routes/petRoute');
 const userRoute = require('./Routes/userRoute');
 const commentRoute = require('./Routes/commentRoute');
 const formRoute = require('./Routes/formRouter');
-const createPayment = require('./Routes/petPayment')
+const createPayment = require('./Routes/petPayment');
 
 const server = express();
 
@@ -25,7 +24,7 @@ server.use('/comment', commentRoute);
 server.use('/form', formRoute);
 server.use('/pet', petRoute);
 server.use('/user', userRoute);
-server.use('/payment', createPayment)
+server.use('/payment', createPayment);
 
 module.exports = {
   server
