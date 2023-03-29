26fd84934ffcb150026e938d71983b831afbd32f
joacorojasmoschini <joaco.vla@gmail.com>
2023-03-28 21:23:27 -0300

    feat: Payment CRUD

--- src/Controllers/paymentController.js
+++ src/Controllers/paymentController.js
@@ -1,14 +1,14 @@
 const mercadopago = require('mercadopago');
 const Payment = require('../Models/paymentModel');
 
-const pet = {};
+const payment = {};
 
 // // http://localhost:3001/payment esta es la ruta
 
-pet.createPayment = async (req, res) => {
+payment.createPayment = async (req, res) => {
   try {
     const { products } = req.body;
-    const user = req.userId;
+    const payment = req.userId;
     const items = products.map((product, index) => ({
       id: index + 1,
       title: product.title,
@@ -18,9 +18,9 @@ pet.createPayment = async (req, res) => {
       category_id: 'art',
       quantity: 1,
       unit_price: product.price
-      
+
     }));
-    
+
     const preference = {
       items,
       back_urls: {
@@ -33,7 +33,7 @@ pet.createPayment = async (req, res) => {
     };
     const response = await mercadopago.preferences.create(preference);
     const newPayment = new Payment({
-      id: user,
+      id: payment,
       amount: products.reduce((total, product) => total + product.price, 0),
       title: products.map(product => product.title).join(', ')
     });
@@ -45,4 +45,80 @@ pet.createPayment = async (req, res) => {
   }
 };
 
-module.exports = pet;
+payment.getAllPayments = async (req, res) => {
+  try {
+    const allPayments = await Payment.find();
+
+    return res.status(200).json({
+      allPayments
+    });
+  } catch (error) {
+    return res.state(400).json({
+      msg: 'Ocurrio un problema, intentalo nuevamente.'
+    });
+  }
+};
+
+payment.getPayment = async (req, res) => {
+  try {
+    const payment = await Payment.findById(req.params.id);
+    if (payment) {
+      return res.status(404).json({
+        msg: 'The payment not found.'
+      });
+    } else {
+      return res.status(200).json({
+        payment
+      });
+    }
+  } catch {
+    return res.state(400).json({
+      msg: 'Ocurrio un problema, intentalo nuevamente.'
+    });
+  }
+};
+
+payment.deletePayment = async (req, res) => {
+  const payment = await Payment.findById(req.params.id);
+  console.log(payment);
+  if (payment) {
+    try {
+      const deletepayment = await Payment.findByIdAndDelete(req.params.id);
+      return res.status(200).json({
+        msg: 'payment eliminado correctamente.',
+        deletepayment
+      });
+    } catch {
+      return res.status(400).json({
+        msg: 'Ocurrio un problema, intentalo nuevamente.'
+      });
+    }
+  } else {
+    return res.status(404).json({
+      msg: 'The payment not found.'
+    });
+  }
+};
+
+payment.updatePayment = async (req, res) => {
+  const payment = await Payment.findById(req.params.id);
+  if (payment) {
+    try {
+      let updatepayment = await Payment.findByIdAndUpdate(req.params.id, req.body);
+      updatepayment = req.body;
+      return res.status(200).json({
+        msg: 'Actualizado correctamente.',
+        updatepayment
+      });
+    } catch {
+      return res.status(400).json({
+        msg: 'Ocurrio un problema, intentalo nuevamente.'
+      });
+    }
+  } else {
+    return res.status(404).json({
+      msg: 'The payment not found.'
+    });
+  }
+};
+module.exports = payment;
