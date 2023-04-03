63cb6b6b3639e569b76f6dbabb560aed09526bde
MadridSelene <seleneailenmadrid@gmail.com>
2023-04-03 17:36:28 -0300

    fix

--- src/Models/comment.js
+++ src/Models/comment.js
@@ -1,13 +1,25 @@
 const { Schema, model } = require('mongoose');
 
 const comment = new Schema({
+  idPet: {
+    ref: 'Pet',
+    type: Schema.Types.ObjectId
+  },
   idUser: {
     ref: 'User',
     type: Schema.Types.ObjectId
   },
+  stars: {
+    type: Number,
+    required: true
+  },
   comments: {
     type: String,
     max: 150,
+    required: true
+  },
+  image: {
+    type: String,
     required: true
   },
   createAt: {
